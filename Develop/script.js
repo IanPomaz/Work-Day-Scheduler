52b1d85f01f946ce1e101c3426bc2b95213cb585
IanPomaz <128009980+IanPomaz@users.noreply.github.com>
2023-09-12 17:16:18 -0400

    Color changes based on time of day

--- Develop/script.js
+++ Develop/script.js
@@ -19,12 +19,48 @@ saveButtons.forEach((saveButton, index) => {
 })
 
 dayjs().format()
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
 
-  // TODO: Add code to display the current date in the header of the page.
+
+
+
+
+function displayTodaysDate() {
+  //current date using day.js
+  const currentDate = dayjs().format("dddd, MMMM D");
+  //creates new element for the current date
+  const dateElement = document.createElement("h3");
+  //creates the text to display it
+  dateElement.innerHTML = `Today's date is ${currentDate}`;
+//pulls the element where we will put todays date
+  const currentDayElement = document.getElementById("currentDay");
+  currentDayElement.insertAdjacentElement("afterend", dateElement);
+}
+//displays everything
+displayTodaysDate()
+
+function changeBlockColor() {
+//compare all the id's of each div to the current hour
+  //current hour
+  const currentHour = dayjs().hour();
+  //loop through all the html divs and extract the hour of each div\
+  for (let i = 1; i <= 24; i++) {
+    const blockElement = document.getElementById(`hour-${i}`);
+    //splits the attribute and retrieves the hour from the array
+    const blockHour = parseInt(blockElement.id.split('-')[1]);
+  //if block hour is before the current hour
+    if (blockHour < currentHour) {
+      blockElement.classList.add('past');
+      blockElement.classList.remove('present', 'future');
+    } else if (blockHour === currentHour) {
+      blockElement.classList.add('present');
+      blockElement.classList.remove('past', 'future');
+    } else {
+      blockElement.classList.add('future');
+      blockElement.classList.remove('past', 'present');
+    }
+  }
+}
+changeBlockColor()
+
+
 });
